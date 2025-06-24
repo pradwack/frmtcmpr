67a9908e00b193b846316e58fbaf1ecf735c6ac6
pradwack <pradnya.wakchaure@gmail.com>
2025-06-23 22:26:26 -0400

    fix: remove fira-code import to fix Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "@fontsource/fira-code"; // Optional, improves vibe
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
